37c4e75603aec2c7a03ea1e6cb2938c56c22ae77
lazyowl <lazyowl@users.noreply.github.com>
2015-02-04 15:05:04 -0500

    changed file cache to now index by user, rather than file

--- cache/filesCache.go
+++ cache/filesCache.go
@@ -2,50 +2,31 @@ package cache
 
 import "yank/fileManager"
 
-type FileCacheValue struct {
-	Names map[string][]string			// map of name -> users
-	ChunkOwners map[uint][]string	// map of chunk id -> list of users
-	MergedBitVector *fileManager.BitVector
+type UserFileCache struct {
+	cache map[string][]fileManager.MyFile
 }
 
-func newFileCacheValue() FileCacheValue {
-	fv := FileCacheValue{}
-	fv.Names = make(map[string][]string)
-	fv.ChunkOwners = make(map[uint][]string)
-	temp := fileManager.BitVectorZero()
-	fv.MergedBitVector = &temp
-	return fv
+func NewUserFileCache() *UserFileCache {
+	uf := UserFileCache{}
+	uf.cache = make(map[string][]fileManager.MyFile)
+	return &uf
 }
 
-type FileListCache struct {
-	cache map[string]FileCacheValue
-}
-
-func NewFileListCache() *FileListCache {
-	f := FileListCache{}
-	f.cache = make(map[string]FileCacheValue)
-	return &f
-}
-
-func (f *FileListCache) PutChunk(hash string, chunkPos uint, user string) {
-	if _, found := f.cache[hash]; !found {
-		f.cache[hash] = newFileCacheValue()
+func (uf *UserFileCache) Put(user string, f fileManager.MyFile) {
+	_, found := uf.cache[user]
+	if !found {
+		uf.cache[user] = []fileManager.MyFile{}
 	}
-	f.cache[hash].ChunkOwners[chunkPos] = append(f.cache[hash].ChunkOwners[chunkPos], user)
-	f.cache[hash].MergedBitVector.SetBit(chunkPos)
+	uf.cache[user] = append(uf.cache[user], f)
 }
 
-func (f *FileListCache) PutName(hash string, name string, user string) {
-	if _, found := f.cache[hash]; !found {
-		f.cache[hash] = newFileCacheValue()
+func (uf *UserFileCache) GetAll() map[string][]fileManager.MyFile {
+	return uf.cache
+}
+
+func (uf *UserFileCache) ClearUser(user string) {
+	_, found := uf.cache[user]
+	if found {
+		delete(uf.cache, user)
 	}
-	f.cache[hash].Names[name] = append(f.cache[hash].Names[name], user)
-}
-
-func (f *FileListCache) GetAll() map[string]FileCacheValue {
-	return f.cache
-}
-
-func (f *FileListCache) Get(hash string) FileCacheValue {
-	return f.cache[hash]
 }
