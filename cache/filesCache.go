e518114df2436f2b5cf8cc47d063bf069cc6077a
lazyowl <lazyowl@users.noreply.github.com>
2015-02-06 23:01:01 -0500

    changed cache into map of maps for easier lookup

--- cache/filesCache.go
+++ cache/filesCache.go
@@ -3,24 +3,24 @@ package cache
 import "yank/fileManager"
 
 type UserFileCache struct {
-	cache map[string][]fileManager.MyFile
+	cache map[string]map[string]fileManager.MyFile	// user -> (hash -> list of myfiles)
 }
 
 func NewUserFileCache() *UserFileCache {
 	uf := UserFileCache{}
-	uf.cache = make(map[string][]fileManager.MyFile)
+	uf.cache = make(map[string]map[string]fileManager.MyFile)
 	return &uf
 }
 
 func (uf *UserFileCache) Put(user string, f fileManager.MyFile) {
 	_, found := uf.cache[user]
 	if !found {
-		uf.cache[user] = []fileManager.MyFile{}
+		uf.cache[user] = make(map[string]fileManager.MyFile)
 	}
-	uf.cache[user] = append(uf.cache[user], f)
+	uf.cache[user][f.FullHash] = f
 }
 
-func (uf *UserFileCache) GetAll() map[string][]fileManager.MyFile {
+func (uf *UserFileCache) GetAll() map[string]map[string]fileManager.MyFile {
 	return uf.cache
 }
 
@@ -29,4 +29,15 @@ func (uf *UserFileCache) ClearUser(user string) {
 	if found {
 		delete(uf.cache, user)
 	}
+}
+
+func (uf *UserFileCache) GetExistingByHash(hash string) map[string]fileManager.MyFile {
+	userMap := make(map[string]fileManager.MyFile)
+	for u, v := range uf.cache {
+		f, found := v[hash]
+		if found {
+			userMap[u] = f
+		}
+	}
+	return userMap
 }
